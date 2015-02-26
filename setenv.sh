e425169852de225fe2cf25c0cad3ea00e0cc6279
raul wang <raulwang71979@gmail.com>
2015-02-26 17:15:40 +0800

    @20150226

--- setenv.sh
+++ setenv.sh
@@ -1,13 +1,3 @@
 #! /bin/sh
 
-find . -maxdepth 1 -type f -a ! -iname "setenv.sh" -a ! -iname "*.swp" | awk -F '/' '{print $2}' | xargs -t -r -i cp {} ~/.{}
-
-if [ ! -e ~/.vim ]; then
-	cp -vrf vim ~/.vim
-else
-	rm -vrf ~/.vim; cp -vrf vim ~/.vim
-fi
-
-find . -maxdepth 1 -type d -a ! -iname ".git" -a ! -iname "vim" | awk -F '/' '{print $2}' | xargs -t -r -i cp -rf {} ~/{}
-
-
+rsync -a -v -u --exclude=.git --exclude=setenv* --exclude=*.swp ./ ~/
